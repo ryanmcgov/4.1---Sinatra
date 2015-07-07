c8aceee66e2a6536f503de15d7bda7ffc5acd0ee
eastcoastsb <ryanmcgovern@Ryans-MacBook-Air.local>
2015-07-07 13:32:57 -0400

    fixing broken sass things

--- app.rb
+++ app.rb
@@ -1,1 +1,23 @@
 require 'sinatra'
+
+get '/' do 
+  erb :index, locals: {title: "Home"}
+end
+
+get '/blog_post1' do
+  erb :post_1, locals: {title: "Post 1"}
+end
+
+get '/blog_post2' do
+  erb :post_2, locals: {title: "Post 2"}
+end
+
+get '/post3' do
+  erb :post_3, locals: {title: "Post 3"}
+end
+
+get '/post4' do
+  erb :post_4, locals: {title: "Post 4"}
+end
+
+
